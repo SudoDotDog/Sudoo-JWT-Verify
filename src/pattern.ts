af2488aff1f24e073f7280e4b8b168378706816e
WMXPY <wm@mengw.io>
2021-06-13 15:29:23 -0500

    implkement jwt header pattern

---
+++ src/pattern.ts
@@ -0,0 +1,41 @@
+/**
+ * @author WMXPY
+ * @namespace JWTVerify
+ * @description Pattern
+ */
+
+import { createIntegerPattern, createStrictMapPattern, createStringPattern, MapPattern, Pattern } from "@sudoo/pattern";
+
+export const createJWTHeaderPattern = (headerPattern: Record<string, Pattern>, options?: Pick<MapPattern, "optional" | "generate">): Pattern => {
+
+    return createStrictMapPattern({
+
+        alg: createStringPattern(),
+        typ: createStringPattern(),
+        aud: createStringPattern({
+            optional: true,
+        }),
+        exp: createIntegerPattern({
+            optional: true,
+        }),
+        jti: createStringPattern({
+            optional: true,
+        }),
+        iat: createIntegerPattern({
+            optional: true,
+        }),
+        iss: createStringPattern({
+            optional: true,
+        }),
+        nbf: createIntegerPattern({
+            optional: true,
+        }),
+        sub: createStringPattern({
+            optional: true,
+        }),
+        ver: createStringPattern({
+            optional: true,
+        }),
+        ...headerPattern,
+    }, options);
+};
