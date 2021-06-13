ed216a5b36830888854ed42c4bc5b7166204c6f0
WMXPY <wm@mengw.io>
2021-06-13 15:24:16 -0500

    add verifier verifu function

--- src/verifier.ts
+++ src/verifier.ts
@@ -4,21 +4,23 @@
  * @description Verifier
  */
 
+import { JWTJoinedHeader } from "@sudoo/jwt-config";
 import { Pattern } from "@sudoo/pattern";
+import { createVerifyResult, Verifier, VerifyResult } from "@sudoo/verify";
 
-export class JWTVerifier {
+export class JWTVerifier<Header extends Record<string, any> = any, Body extends Record<string, any> = any> {
 
-    public static onlyHeader(pattern: Pattern): JWTVerifier {
+    public static onlyHeader<Header extends Record<string, any> = any, Body extends Record<string, any> = any>(pattern: Pattern): JWTVerifier<Header, Body> {
 
         return new JWTVerifier(pattern);
     }
 
-    public static onlyBody(pattern: Pattern): JWTVerifier {
+    public static onlyBody<Header extends Record<string, any> = any, Body extends Record<string, any> = any>(pattern: Pattern): JWTVerifier<Header, Body> {
 
         return new JWTVerifier(undefined, pattern);
     }
 
-    public static headerAndBody(headerPattern: Pattern, bodyPattern: Pattern): JWTVerifier {
+    public static headerAndBody<Header extends Record<string, any> = any, Body extends Record<string, any> = any>(headerPattern: Pattern, bodyPattern: Pattern): JWTVerifier<Header, Body> {
 
         return new JWTVerifier(headerPattern, bodyPattern);
     }
@@ -30,5 +32,41 @@ export class JWTVerifier {
 
         this._headerPattern = headerPattern;
         this._bodyPattern = bodyPattern;
+    }
+
+    public validateHeader(header: JWTJoinedHeader<Header>): boolean {
+
+        const result: VerifyResult = this.verifyHeader(header);
+        return result.succeed;
+    }
+
+    public verifyHeader(header: JWTJoinedHeader<Header>): VerifyResult {
+
+        if (!this._headerPattern) {
+            return createVerifyResult(true);
+        }
+
+        const verifier: Verifier = Verifier.create(this._headerPattern);
+        const result: VerifyResult = verifier.verify(header);
+
+        return result;
+    }
+
+    public validateBody(body: Body): boolean {
+
+        const result: VerifyResult = this.verifyBody(body);
+        return result.succeed;
+    }
+
+    public verifyBody(body: Body): VerifyResult {
+
+        if (!this._bodyPattern) {
+            return createVerifyResult(true);
+        }
+
+        const verifier: Verifier = Verifier.create(this._bodyPattern);
+        const result: VerifyResult = verifier.verify(body);
+
+        return result;
     }
 }
